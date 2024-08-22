3240b087670a5223a55d4a01f39900d798ca70c7
jade <jhonnatanwfc@live.com>
2024-08-22 04:48:02 -0300

    drop & display word

--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
-
 function love.load()
     package.path = package.path .. ";/home/jade/Code/love2d-helper/?.lua"
     helper = require "helper"
@@ -6,8 +5,10 @@ function love.load()
     love.graphics.setBackgroundColor(helper:get_color('phthalo_green'))
     words = getTextData()
     math.randomseed(os.time())
-    current_word = dropWord()
+    current_word = dropWord(4)
     font = love.graphics.newFont("fonts/playfairDisplay_variableFont.ttf", 24)
+    color_pressed = {1, 0.5, 0}
+    color_normal = {1, 1, 1}
 end
 
 function getTextData()
@@ -22,22 +23,36 @@ end
 
 function dropWord(length)
     length = length or 3
-    local word
+    local word = {text = "", final=0}
     repeat
-        word = words[math.random(#words)]
-    until #word == length
+        word.text = words[math.random(#words)]
+    until #word.text == length
     return word
 end
 
+function displayWord(word)
+    if word.final >= 1 then
+        love.graphics.print({color_pressed, string.sub(word.text, 1, word.final),
+                             color_normal, string.sub(word.text, word.final + 1)}, font, settings.win_width / 2, 0)
+    else 
+        love.graphics.print({color_normal, word.text}, font, settings.win_width / 2, 0)
+    end
+end
+
 function love.draw()
-    love.graphics.print({{1, 0.5, 0}, current_word}, font, settings.win_width / 2, 0)
+    displayWord(current_word)
+end
+
+function checkPress(key)
+    local next = current_word.final + 1
+    if key == string.sub(current_word.text, next, next) then
+        current_word.final = next
+    end
 end
 
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
     end
-    if key == "space" then
-        current_word = dropWord()
-    end
+    checkPress(key)
 end
